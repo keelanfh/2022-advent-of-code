0d6a578badf82cda7e83262c61d5b2fb94793c19
Keelan Fadden-Hopper <info@keelanfh.com>
2022-12-03 13:23:21 +0000

    Refactoring for (slightly) cleaner code

--- 02/main.php
+++ 02/main.php
@@ -2,10 +2,40 @@
 
 require "helpers/helpers.php";
 
+$WIN_SCORE = 6;
+$DRAW_SCORE = 3;
+$LOSE_SCORE = 0;
+
 enum Move: int {
     case Rock = 1;
     case Paper = 2;
     case Scissors = 3;
+
+    public function how_to_draw(): Move
+    {
+        return $this;
+    }
+    
+    public function how_to_win(): Move
+    {
+        return match($this)
+        {
+            Move::Rock => Move::Paper,
+            Move::Paper => Move::Scissors,
+            Move::Scissors => Move::Rock,
+        };
+    }
+
+    public function how_to_lose(): Move
+    {
+        return match($this)
+        {
+            Move::Rock => Move::Scissors,
+            Move::Paper => Move::Rock,
+            Move::Scissors => Move::Paper,
+        };
+    }
+
 }
 
 function letter_to_move ($letter) {
@@ -23,38 +53,40 @@ function letter_to_move ($letter) {
 }
 
 function score($mine, $theirs) {
-    $WIN_SCORE = 6;
-    $DRAW_SCORE = 3;
-    $LOSE_SCORE = 0;
+    global $WIN_SCORE, $DRAW_SCORE, $LOSE_SCORE;
 
     if ($mine == $theirs) {
         return $DRAW_SCORE;
-    } if ($mine == Move::Rock) {
-        if ($theirs == Move::Scissors) {
-            return $WIN_SCORE;
-        }
-        if ($theirs == Move::Paper) {
-            return $LOSE_SCORE;
-        }
-    } if ($mine == Move::Paper) {
-        if ($theirs == Move::Rock) {
-            return $WIN_SCORE;
-        }
-        if ($theirs == Move::Scissors) {
-            return $LOSE_SCORE;
-        }
-    } if ($mine == Move::Scissors) {
-        if ($theirs == Move::Paper) {
-            return $WIN_SCORE;
-        }
-        if ($theirs == Move::Rock) {
-            return $LOSE_SCORE;
-        }
+    }
+    
+    switch ($mine) {
+        case Move::Rock:
+            return match($theirs)
+                {
+                    Move::Scissors => $WIN_SCORE,
+                    Move::Paper => $LOSE_SCORE,
+                };
+
+        case Move::Paper:
+            return match($theirs)
+                {
+                    Move::Rock => $WIN_SCORE,
+                    Move::Scissors => $LOSE_SCORE,
+                };
+                
+        case Move::Scissors:
+            return match($theirs)
+                {
+                    Move::Paper => $WIN_SCORE,
+                    Move::Rock => $LOSE_SCORE,
+                };
     }
 }
 
 $array = read_file_to_array("02/input.txt");
 
+
+// Part 1
 $total_score = 0;
 
 foreach($array as $line) {
@@ -67,6 +99,30 @@ foreach($array as $line) {
     $total_score += score($mine, $theirs);
 }
 
-echo $total_score . "\n"
+echo $total_score . "\n";
+
+$mine = 0;
+
+// Part 2
+$total_score = 0;
+
+foreach($array as $line) {
+    $moves = explode(" ", $line);
+
+    $theirs = letter_to_move($moves[0]);
+
+    $mine = match($moves[1])
+        {
+            "X" => $theirs->how_to_lose(),
+            "Y" => $theirs->how_to_draw(),
+            "Z" => $theirs->how_to_win(),
+        };
+
+    $total_score += $mine->value;
+    $total_score += score($mine, $theirs);
+
+}
+
+echo $total_score . "\n";
 
 ?>
